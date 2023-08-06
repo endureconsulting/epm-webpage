37747c6d9aa4650de11156bdba02f4d730f1fe82
endureconsulting <78554883+endureconsulting@users.noreply.github.com>
2023-08-06 15:40:50 -0500

    Delete components directory

--- components/xl-logo-background.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from "react";
-
-export const XlLogoBackground = ({ children }: PropsWithChildren) => (
-  <div className="bg-logo bg-cover bg-fixed bg-left-top bg-no-repeat">
-    {children}
-  </div>
-);
